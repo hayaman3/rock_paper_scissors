5a239e1c397ab9b3066dc05eade64fb33a588752
Kevin <kevin.haya@yahoo.com>
2022-05-31 18:32:29 +0800

    added functions for DOM

--- script.js
+++ script.js
@@ -1,105 +1,46 @@
-// function play(computerSelection, playerSelection){
-//     //compares computer and player selection returns WIN LOSE or TIE
-//     console.log("Computer: "+computerSelection)
-//     console.log("You: "+playerSelection)
-//     //nested if statement style
-//     if(computerSelection == playerSelection){
-//         return "TIE!";
-//     }else{
-//         if(playerSelection=="rock"){
-//             if(computerSelection=="paper"){
-//                 return "YOU LOSE!";
-//             }else{
-//                 return "YOU WIN!"
-//             }
-//         }else if(playerSelection=="paper"){
-//             if(computerSelection=="scissors"){
-//                 return "YOU LOSE!";
-//             }else{
-//                 return "YOU WIN!"
-//             }   
-//         }else if(playerSelection=="scissors"){
-//             if(computerSelection=="rock"){
-//                 return "YOU LOSE!";
-//             }else{
-//                 return "YOU WIN!"
-//             }
-//         }
-//     }
-// }
-
-
-//document.getElementById("clickMe").onclick = game;
-
-//creating new code for ui
-
-
-// function game(){
-//     var player = playerSelected;
-//     var computer= 0;
-//     //while (player!=5 && computer!=5) {
-//         var x = play(computerPlay(),playerSelection())
-//        if(x=="YOU WIN!"){
-//             console.log("YOU WIN!");
-//         }else{
-//             computer++;
-//         }
-//     //}
-//     //console.log("win :"+win);
-//     // //console.log("tie :"+tie);
-//     //console.log("lose :"+lose);
-//     const computerScore = document.getElementById('computerScore')
-//     computerScore.textContent = "Computer: " +computer;
-//     computerScore.append();
-
-//     const playerScore = document.getElementById('playerScore')
-//     playerScore.textContent = "You: " +player;
-//     playerScore.append();
-// }
-
 function show(){
     const show = document.getElementById('hidden');
     show.style.visibility= "visible";
-   // show.append()
 }
 
 //returns random computer (rock, paper or scissors)
 function computerPlay(){
+    var result = ""
     var randomNumber =  Math.floor(Math.random() * 3);
     if(randomNumber == 0){
-        return "rock";
+        result = "rock";
     }else if(randomNumber == 1){
-        return "paper";
+        result = "paper";
     }else{
-        return "scissors";
+        result = "scissors";
     } 
+    compterChoice(result);
+    return result;
 }
+
+var computer = 0;
+var player = 0;
+var tie = 0;
 
 function playerSelection(playerSelected){
     var playerSelected = playerSelected;
-    var computer = 0;
-    var player = 0;
-    //while (player!=5 && computer!=5) {
-        var x = play(computerPlay(),playerSelected)
-       if(x=="YOU WIN!"){
-            console.log("YOU WIN!");
-        }else{
-            computer++;
-        }
-    
-    //console.log("win :"+win);
-    // //console.log("tie :"+tie);
-    //console.log("lose :"+lose);
-    const computerScore = document.getElementById('computerScore')
-    computerScore.textContent = "Computer: " +computer;
-    computerScore.append();
 
-    const playerScore = document.getElementById('playerScore')
-    playerScore.textContent = "You: " +player;
-    playerScore.append();
+    //while (player!=5 && computer!=5) {
+        var result = compare(computerPlay(),playerSelected)
+        if(result=="YOU WIN!"){
+            player++
+            appendResult()
+        }else if(result=="YOU LOSE!"){
+            computer++;
+            appendResult()
+        }else{
+            tie++;
+            appendResult()
+        }
+    //}
 }
 
-function play(computerSelection, playerSelection){
+function compare(computerSelection, playerSelection){
     //compares computer and player selection returns WIN LOSE or TIE
     console.log("Computer: "+computerSelection)
     console.log("You: "+playerSelection)
@@ -127,4 +68,32 @@ function play(computerSelection, playerSelection){
             }
         }
     }
+}
+
+function appendResult(){
+    const computerScore = document.getElementById('computerScore')
+    computerScore.textContent = "Computer: " +computer;
+    computerScore.append();
+
+    const playerScore = document.getElementById('playerScore')
+    playerScore.textContent = "You: " +player;
+    playerScore.append();
+}
+
+function compterChoice(result){
+    var result = result
+    const show = document.getElementById('computerChoice');
+    show.style.visibility= "visible";
+    if(result=="rock"){
+        show.style.backgroundImage = "url('images/rock.png')";
+    }else if(result=="paper"){
+    show.style.backgroundImage = "url('images/paper.png')";
+    }else{
+        show.style.backgroundImage = "url('images/scissors.png')";
+    }
+}
+
+function tie(){
+    const show = document.getElementById('hidden');
+    show.style.visibility= "visible";
 }
